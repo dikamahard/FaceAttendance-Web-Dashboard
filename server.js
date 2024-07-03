3187e49a2c160187e5fb1d74fd317df3f6b26d9d
Dikamahard <ikkyboss@gmail.com>
2024-07-03 23:30:48 +0700

    add feature image upload and delete, integrating storage with database

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { ref as ref_db, set } from 'firebase/database'
+import { onValue, ref as ref_db, set } from 'firebase/database'
 import { ref as ref_storage, uploadBytes, getDownloadURL, listAll, deleteObject } from 'firebase/storage'
 import multer from 'multer'
 
@@ -13,8 +13,8 @@ import dataTes from './tes.js'
 const app = express()
 const PORT = 4353
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')   // set templating engine
+app.use(express.urlencoded({ extended: true }));    // needed to parse body from html form to the server
 
 //app.set('views','./views')
 
@@ -44,10 +44,18 @@ app.get('/upload', async (req, res) => {
     let listPathRef = []
     let listPhotoUrl = []
     let listPhotoId = []
+    let listUserName = []
 
     listResult.items.forEach((item) => {
         listPathRef.push(item.fullPath)
         listPhotoId.push(item.name)
+        const userNameRef = ref_db(db, `tes/${item.name.split('.')[0]}`)
+        onValue(userNameRef, (snapshot) => {
+            const user = snapshot.val()
+            listUserName.push(user.name)
+        })
+    
+
     })
 
     for (const item of listPathRef) {
@@ -59,17 +67,25 @@ app.get('/upload', async (req, res) => {
     console.log(listPathRef)
     console.log(listPhotoId)
 
-    res.render('upload.ejs', {listPathRef: listPathRef, listPhotoUrl: listPhotoUrl, listPhotoId: listPhotoId})
+    res.render('upload.ejs', {listPathRef: listPathRef, listPhotoUrl: listPhotoUrl, listPhotoId: listPhotoId, listUserName: listUserName})
 
 
 })
 
-app.post('/add-data', (req, res) => {
+app.post('/add-data', async (req, res) => {
+
     const data = "tess 1234"
-    set(ref_db(db, 'tesdata'), {
-        tes: data
-    })
-    return res.status(201).send('<p>succes</p>')    // should be pop up
+
+    try {
+        await set(ref_db(db, 'tesdata/2'), {
+            test: data
+        }).then((valu) => console.log(valu))
+        console.log("end method")
+        //return res.status(201).send('<p>succes</p>')    // should be pop up
+    } catch (error) {
+        
+    }
+    
 })
 
 
@@ -80,19 +96,21 @@ app.post('/upload-user', upload.single('image'), async (req, res) => {
 
     try {
         const userId = req.body.userId
+        const userName = req.body.userName
+
         const tesStorageRef = ref_storage(storage, `tes/${userId}.jpg`)
         const metadata = {
             contentType: 'image/png'
         }
 
-        // uploadBytes(tesStorageRef, req.file.buffer, metadata).then((snapshot) => {
-        //     console.log(`Uploaded ${snapshot.ref}`)
-        // })
 
-        // TODO: upload name to realtimedb
         const result = await uploadBytes(tesStorageRef, req.file.buffer, metadata)
-        console.log(`Uploaded ${userId}`)
-        res.redirect('/');
+        const databasePath = `tes/${userId}`
+        await set(ref_db(db, databasePath), {
+            name: userName
+        })
+        console.log(`Uploaded ${userId} ${userName} ${result.metadata.size}`)
+        res.redirect('/upload');
 
     } catch (error) {
         return res.status(400).send(error.message)
